bb3965c41d8c0c57171cb743204aa641998315d1
Krestronic1 <krestronic1@Krestronic1s-Mac-mini.local>
2018-04-16 12:21:16 +1000

    Chekin

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  private currentNumberOfAdults = 0;
+  private currentNumberOfKids = 0;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  //increase number of adults
+  private incrementAdult (){
+    this.currentNumberOfAdults++;
+    console.log(this.currentNumberOfAdults);
+  }
+
+  // decrese number of adults
+  private decrementAdult (){
+    this.currentNumberOfAdults--;
+  }
+
+  // increase number of kids
+  private incrementKid (){
+    this.currentNumberOfKids++;
+    console.log(this.currentNumberOfKids);
+  }
+
+  // decrease number of kids
+  private decrementKid (){
+    this.currentNumberOfKids--;
   }
 
 }
