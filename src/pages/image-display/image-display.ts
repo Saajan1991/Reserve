2a4e18c19598c7f182e6fc00f7876f6bd14e5194
Krestronic1 <krestronic1@Krestronic1s-Mac-mini.local>
2018-04-17 15:03:38 +1000

    test

--- src/pages/image-display/image-display.ts
+++ src/pages/image-display/image-display.ts
@@ -11,27 +11,28 @@ export class ImageDisplayPage {
 
   TotalNumberOfAdults = 0;
   TotalNumberOfKids = 0;
-  items = [];
+  adults = [];
+  kids = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // this.base64Image = navParams.get('image');
-    // alert(this.base64Image);
-
+    alert(navParams.get('image'));
+    this.base64Image = "http://blog.inf.ed.ac.uk/atate/files/2015/11/img-avatars-new-300x207.png";
     this.TotalNumberOfAdults = navParams.get('adults');
     this.TotalNumberOfKids = navParams.get('kids');
     alert(this.TotalNumberOfAdults);
     this.adult(this.TotalNumberOfAdults);
-    // this.kid(this.TotalNumberOfKids);
+    this.kid(this.TotalNumberOfKids);
 
   }
 
   adult(adultNumber) {
-    this.items = Array(adultNumber).fill(0).map((x, i) => i);
+    this.adults = Array(adultNumber).fill(0).map((x, i) => i);
   }
 
-  // kid(kidNumber){
-  //   this.items = Array(kidNumber).fill(0).map((x,i)=>i);
-  // }
+  kid(kidNumber){
+    this.kids = Array(kidNumber).fill(0).map((x,i)=>i);
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ImageDisplayPage');
