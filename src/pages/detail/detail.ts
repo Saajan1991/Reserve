5a355aad48d32fce1fcfa12846ad72f5c937de2d
Krestronic1 <krestronic1@hello.local>
2018-05-07 11:25:17 +1000

    face detection

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -11,6 +11,7 @@ import firebase from 'firebase';
 })
 export class DetailPage {
 
+  downloadURL: any;
   TotalNumberOfAdults = 0;
   TotalNumberOfKids = 0;
 
@@ -49,29 +50,42 @@ export class DetailPage {
 
     //open camera in device
     this.camera.getPicture(options).then((imageData) => {
-      this.vision.getLabels(imageData).subscribe((result) => {
-        // alert("Save success");
-        this.items = JSON.stringify(result);
-        this.items = JSON.parse(this.items);
-        // alert(this.items);
-        this.labels = this.items.responses[0].labelAnnotations;
-        // alert(this.labels);
+      this.base64image = 'data:image/jpeg;base64,' + imageData;
+      let downloadUrl = this.upload(this.base64image);
 
-        this.base64image = 'data:image/jpeg;base64,' + imageData;
-
-        //send data to imageDisplayPage
-        this.navCtrl.push(ImageDisplayPage, {
-          image: this.base64image,
-          adults: this.TotalNumberOfAdults,
-          kids: this.TotalNumberOfKids,
-          labels: this.labels
-        });
-      }, err => {
-        alert(err);
-      });
     }, err => {
       alert(err);
     });
+  }
+
+  upload(imageDataResult) {
+    let storageRef = firebase.storage().ref();
+    this.filename = Math.floor(Date.now() / 1000);
+    alert("Filename " + this.filename);
+    const imageRef = storageRef.child(`images/${this.filename}.jpg`);
+    imageRef.putString(imageDataResult, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
+      this.vision.getFaces(this.filename).subscribe((result) => {
+        alert("Success get face");
+        this.items = JSON.parse(JSON.stringify(result));
+        this.labels = this.items.responses[0].faceAnnotations;
+        alert(this.labels);
+        if (this.items != undefined) {
+          alert("Total Face Detected: " + this.items.responses[0].faceAnnotations.length);
+          this.labels = this.items.responses[0].faceAnnotations;
+        }
+        else {
+          alert("Face Not Detected");
+        }
+        alert(this.labels + "item to display");
+      }, err => {
+        alert(err);
+      });
+
+      // alert("label");
+    }).catch(function (err) {
+      return err
+    });
+    return this.downloadURL;
   }
 
 }
