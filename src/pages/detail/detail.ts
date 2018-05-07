0465492c2f126617387c1ef5877a6e20787181f8
Krestronic1 <krestronic1@hello.local>
2018-05-07 11:06:22 +1000

    face detection

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -3,7 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { ImageDisplayPage } from '../image-display/image-display';
 import { GoogleCloudVisionServiceProvider } from '../../providers/google-cloud-vision-service/google-cloud-vision-service';
-
 import firebase from 'firebase';
 
 @Component({
@@ -12,9 +11,6 @@ import firebase from 'firebase';
 })
 export class DetailPage {
 
-  downloadURL: any;
-  filename;
-  items1: { responses: {} };
   TotalNumberOfAdults = 0;
   TotalNumberOfKids = 0;
 
@@ -22,6 +18,8 @@ export class DetailPage {
 
   labels: { response: {} };
   items;
+
+  filename;
 
   constructor(
     public navCtrl: NavController,
@@ -36,130 +34,125 @@ export class DetailPage {
     console.log('ionViewDidLoad DetailPage');
   }
 
-  // takePhoto() {
-  //   const options: CameraOptions = {
-  //     quality: 100,
-  //     destinationType: this.camera.DestinationType.DATA_URL,
-  //     encodingType: this.camera.EncodingType.PNG,
-  //     mediaType: this.camera.MediaType.PICTURE,
-  //     correctOrientation: true
-  //   }
 
-  //   this.camera.getPicture(options).then((imageData) => {
-  //     this.base64image = 'data:image/jpeg;base64,' + imageData;
-  //     this.navCtrl.push(ImageDisplayPage, { image: this.base64image, adults: this.TotalNumberOfAdults, kids: this.TotalNumberOfKids });
-  //   }, (err) => {
-  //     // Handle error
-  //   });
-  // }
+  takePhoto() {
 
-  //set options for camera
-  public cameraOptions() {
     const options: CameraOptions = {
       quality: 100,
       targetHeight: 500,
       targetWidth: 500,
       destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
     }
-    return options;
-  }
-
-
-  takePhoto() {
-    let options = this.cameraOptions();
 
     //open camera in device
     this.camera.getPicture(options).then((imageData) => {
+      this.vision.getLabels(imageData).subscribe((result) => {
+        // alert("Save success");
+        this.items = JSON.stringify(result);
+        this.items = JSON.parse(this.items);
+        // alert(this.items);
+        this.labels = this.items.responses[0].labelAnnotations;
+        // alert(this.labels);
 
-      let imageDataResult = 'data:image/jpeg;base64,' + imageData;
-      let downloadURL = this.upload(imageDataResult);
+        this.base64image = 'data:image/jpeg;base64,' + imageData;
 
-      //send data to imageDisplayPage
-      this.navCtrl.push(ImageDisplayPage, {
-        image: imageDataResult,
-        adults: this.TotalNumberOfAdults,
-        kids: this.TotalNumberOfKids
+        //send data to imageDisplayPage
+        this.navCtrl.push(ImageDisplayPage, {
+          image: this.base64image,
+          adults: this.TotalNumberOfAdults,
+          kids: this.TotalNumberOfKids,
+          labels: this.labels
+        });
+      }, err => {
+        alert(err);
       });
-
-
-      //vision Api call 
-      // this.vision.getLabels(imageData).subscribe((result) => {
-
-      //   this.items = JSON.stringify(result);
-      //   this.items = JSON.parse(this.items);
-      //   this.labels = this.items.responses[0].labelAnnotations;
-
-      //   //convert imageData to base64Image
-      //   this.base64image = 'data:image/jpeg;base64,' + imageData;
-
-      //   //send data to imageDisplayPage
-      //   this.navCtrl.push(ImageDisplayPage, {
-      //     image: this.base64image,
-      //     adults: this.TotalNumberOfAdults,
-      //     kids: this.TotalNumberOfKids,
-      //     labels: this.labels
-      //   });
-      // }, err => {
-      //   alert(err);
-      // });
     }, err => {
       alert(err);
     });
   }
-
-
-  //google vision api to get faces
-  private faceDetection(filename) {
-alert(filename + "faceDetectyion");
-    this.vision.getFaces(filename).subscribe((result) => {
-      alert(result);
-      this.items = JSON.parse(JSON.stringify(result));
-
-      this.items1 = this.items.responses[0].faceAnnotations;
-
-      //total number of faces in the image
-      if (this.items != undefined) {
-        alert("Total Face Detected: " + this.items.responses[0].faceAnnotations.length);
-        this.items1 = this.items.responses[0].faceAnnotations;
-      }
-      else {
-        alert("Face Not Detected");
-      }
-
-      alert(this.items1 + "item to display");
-    }, err => {
-      alert(err);
-    });
-  }
-
-  //upload file to firebase
-  upload(imageDataResult) {
-    alert("Upload" + imageDataResult);
-    let storageRef = firebase.storage().ref();
-    alert("Storage ref " + storageRef);
-
-    // Create a timestamp as filename
-    this.filename = Math.floor(Date.now() / 1000);
-    alert(this.filename);
-    // this.filename = "HelloImage";
-
-    // Create a reference to 'images/todays-date.jpg'
-    const imageRef = storageRef.child(`images/${this.filename}.jpg`);
-
-    alert("Image ref " + imageRef);
-    imageRef.putString(imageDataResult, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
-      alert("Save Success");
-      // Do something here when the data is succesfully uploaded!
-      this.faceDetection(this.filename);
-
-      // alert("label");
-    }).catch(function (err) {
-      return err
-    });
-    return this.downloadURL;
-  }
-
 
 }
+
+
+
+
+// import { Component } from '@angular/core';
+// import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// import { Camera, CameraOptions } from '@ionic-native/camera';
+// import { ImageDisplayPage } from '../image-display/image-display';
+// import { GoogleCloudVisionServiceProvider } from '../../providers/google-cloud-vision-service/google-cloud-vision-service';
+// import firebase from 'firebase';
+
+// @Component({
+//   selector: 'page-detail',
+//   templateUrl: 'detail.html',
+// })
+// export class DetailPage {
+
+//   TotalNumberOfAdults = 0;
+//   TotalNumberOfKids = 0;
+
+//   public base64image: string; 
+
+//   labels: { response: {} };
+//   items;
+
+//   constructor(
+//     public navCtrl: NavController,
+//     public navParams: NavParams,
+//     private camera: Camera,
+//     private vision: GoogleCloudVisionServiceProvider) {
+//     this.TotalNumberOfAdults = navParams.get('adults');
+//     this.TotalNumberOfKids = navParams.get('kids');
+//   }
+
+//   ionViewDidLoad() {
+//     console.log('ionViewDidLoad DetailPage');
+//   }
+
+
+//   takePhoto() {
+
+//     const options: CameraOptions = {
+//       quality: 100,
+//       targetHeight: 500,
+//       targetWidth: 500,
+//       destinationType: this.camera.DestinationType.DATA_URL,
+//       encodingType: this.camera.EncodingType.PNG,
+//       mediaType: this.camera.MediaType.PICTURE,
+//       correctOrientation: true
+//     }
+
+//     //open camera in device
+//     this.camera.getPicture(options).then((imageData) => {
+//       this.vision.getLabels(imageData).subscribe((result) => {
+//         // alert("Save success");
+//         this.items = JSON.stringify(result);
+//         this.items = JSON.parse(this.items);
+//         // alert(this.items);
+//         this.labels = this.items.responses[0].labelAnnotations;
+//         // alert(this.labels);
+
+//         this.base64image = 'data:image/jpeg;base64,' + imageData;
+
+//         //send data to imageDisplayPage
+//         this.navCtrl.push(ImageDisplayPage, {
+//           image: this.base64image,
+//           adults: this.TotalNumberOfAdults,
+//           kids: this.TotalNumberOfKids,
+//           labels: this.labels
+//         });
+//       }, err => {
+//         alert(err);
+//       });
+//     }, err => {
+//       alert(err);
+//     });
+//   }
+
+// }
+
+
