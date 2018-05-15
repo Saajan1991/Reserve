914d2ad582584a52244fb4bce066534165f436d3
Krestronic1 <krestronic1@hello.local>
2018-05-15 15:11:51 +1000

    face detail page added

--- src/pages/face-detail/face-detail.ts
+++ src/pages/face-detail/face-detail.ts
@@ -9,14 +9,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class FaceDetailPage {
 
   slidesPerView = 1;
-
+  a;
+  i= 0;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     let faces = navParams.get('faces');
     let imageData = navParams.get('image');
 
-    console.log(faces);
-    console.log(imageData);
+    alert(faces);
 
+    let f1 = faces.responses[0].faceAnnotations;
+
+      for (let face of f1) {
+        let faceVertices = face.boundingPoly.vertices;
+        this.a = faceVertices[0];
+        if (this.a.x == undefined) {
+          this.a.x = 0;
+        }
+
+        let styles = {
+          'object-fit': 'none',
+          'object-position': '-' + this.a.x + 'px -' + this.a.y + 'px',
+          "width": "200px",
+          "height": "200px"
+        };
+        faces[this.i].style = styles;
+        this.i++;
+      }
+      alert(faces[0].style);
   }
 
   ionViewDidLoad() {
