9f5744d77237cd405d9b97213d727754155dd548
Krestronic1 <krestronic1@hello.local>
2018-06-25 11:12:51 +1000

    update image

--- src/pages/add-business/add-business.ts
+++ src/pages/add-business/add-business.ts
@@ -127,6 +127,15 @@ export class AddBusinessPage {
   // }
 
   getImage() {
+    // const options: CameraOptions = {
+    //   quality: 50,
+    //   destinationType: this.camera.DestinationType.DATA_URL,
+    //   encodingType: this.camera.EncodingType.JPEG,
+    //   mediaType: this.camera.MediaType.PICTURE,
+    //   correctOrientation: true,
+    //   sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    // }
+
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
