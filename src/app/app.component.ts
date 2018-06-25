23137d61aa1b6318238d2e4cc625905022b08258
Krestronic1 <krestronic1@hello.local>
2018-06-25 11:05:37 +1000

    update image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+      // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
     });
 
     // this.pages = [
